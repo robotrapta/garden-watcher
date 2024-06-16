a0935d9a6f75c633934a341e7a1f41edb5c85586
Leo Dirac <leo@groundlight.ai>
2024-06-16 12:29:09 -0700

    Just a shell

---
+++ app.py
@@ -0,0 +1,11 @@
+from groundlight import Groundlight
+
+def main():
+    print("Deerbark connecting to Groundlight")
+    gl = Groundlight()
+    print("Connected to Groundlight")
+    print(gl)
+
+
+if __name__ == "__main__":
+    main()
