f7103bfde9b733959e5425010af97674dc02ec8b
Leo Dirac <leo@groundlight.ai>
2024-06-16 18:06:31 -0700

    Camera logic.

--- app.py
+++ app.py
@@ -2,23 +2,30 @@
 import time
 
 from groundlight import Groundlight
+from imgcat import imgcat
+import framegrab
 import pygame
 
+def next_image():
+    global camera
+    if camera is None:
+        camera = framegrab.FrameGrabber.create_grabber_yaml("./framegrab.yaml")
+    return camera.grab()
+
 def main():
-    print("Deerbark connecting to Groundlight")
     gl = Groundlight()
-    print("Connected to Groundlight")
-    print(gl)
 
     pygame.init()
     pygame.mixer.init()
     SOUND = pygame.mixer.Sound("./dog-barking.mp3")
 
     while True:
+        img = next_image()
+        imgcat(img)
         print("barking!")
-        SOUND.play()
+        #SOUND.play()
         print("waiting")
-        time.sleep(10)
+        #time.sleep(10)
 
 if __name__ == "__main__":
     main()
